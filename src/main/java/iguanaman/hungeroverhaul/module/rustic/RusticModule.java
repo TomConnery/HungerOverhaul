6d1d9a8310545a8fdd6a886b9f6ce30f0428ba46
TomConn <l.dapo@gmx.net>
2018-10-01 01:05:46 +0200

    - added logging for registered plants (currently just the number, no details) - switched from Block- to Class-based registration as a speculative fix for the GrowthModule not providing registered modifiers for the blocks

--- src/main/java/iguanaman/hungeroverhaul/module/rustic/RusticModule.java
+++ src/main/java/iguanaman/hungeroverhaul/module/rustic/RusticModule.java
@@ -4,6 +4,7 @@ import iguanaman.hungeroverhaul.module.growth.PlantGrowthModule;
 import iguanaman.hungeroverhaul.module.growth.modification.PlantGrowthModification;
 import net.minecraft.block.Block;
 import rustic.common.blocks.ModBlocks;
+import static iguanaman.hungeroverhaul.HungerOverhaul.log;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -21,7 +22,7 @@ public class RusticModule {
 
 
 
-    public static void init() {
+    public static void postInit() {
 
         random.setSeed(2 ^ 16 + 2 ^ 8 + (4 * 3 * 271));
 
@@ -31,10 +32,9 @@ public class RusticModule {
         crop_berry = ModBlocks.WILDBERRY_BUSH;     //temp / humid
 
 
-
-        PlantGrowthModule.registerPlantGrowthModifiers(generatePlantMap());
-
-
+        Map<Class<? extends Block>, PlantGrowthModification> plantMap = generatePlantMap();
+        PlantGrowthModule.registerPlantGrowthModifiersClasses(plantMap);
+        log.info(String.format("Registered %d Rustic-plants in the growth module", plantMap.entrySet().size()));
     }
 
 
@@ -42,27 +42,27 @@ public class RusticModule {
      * Simply generates the plantmap and returns the instance
      * @return the plantMap with a plant -> growthMod mapping
      */
-    private static Map<Block, PlantGrowthModification> generatePlantMap() {
+    private static Map<Class<? extends Block>, PlantGrowthModification> generatePlantMap() {
 
-        Map<Block, PlantGrowthModification> plantMap = new HashMap<>();
+        Map<Class<? extends Block>, PlantGrowthModification> plantMap = new HashMap<>();
 
-        plantMap.put(crop_tomato,
+        plantMap.put(crop_tomato.getClass(),
                 PlantGrowthModification.HUMID_GROWTH_MODIFIER
                 .withTypeModifiersOf(PlantGrowthModification.TROPICAL_GROWTH_MODIFIER)
         );
 
-        plantMap.put(crop_chili,
+        plantMap.put(crop_chili.getClass(),
                 PlantGrowthModification.HUMID_GROWTH_MODIFIER
                 .withTypeModifiersOf(PlantGrowthModification.TROPICAL_GROWTH_MODIFIER)
 
         );
 
-        plantMap.put(crop_grape,
+        plantMap.put(crop_grape.getClass(),
                 PlantGrowthModification.HUMID_GROWTH_MODIFIER
                 .withTypeModifiersOf(PlantGrowthModification.TROPICAL_GROWTH_MODIFIER)
         );
 
-        plantMap.put(crop_berry,
+        plantMap.put(crop_berry.getClass(),
                 PlantGrowthModification.HUMID_GROWTH_MODIFIER
                 .withTypeModifiersOf(PlantGrowthModification.TROPICAL_GROWTH_MODIFIER)
         );
